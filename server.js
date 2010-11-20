f4c7b467b59560ed497d8511ec4e0e175f5b0662
megalonyx <d@nieltiggemann.de>
2010-11-20 20:10:52 +0100

    improving dispatch

--- server.js
+++ server.js
@@ -11,24 +11,54 @@ var m = {
 	, bodyend: '</body></html>'
 };
 
-m.dispatch = function (req, res) {
+m.title = function (str) {
+	return '<title>' + str + '</title>';
+}
+
+m.root = function (req) {
 	var   i
 		, outstr = 
 			  m.headbegin
-			+ '<title>Test</title>'
+			+ '<title>Request</title>'
 			+ m.headend
 			+ m.bodybegin
 		;
-	outstr += '<h1>' + url.parse(req.url).pathname + '</h1><p>';
+	outstr += m.title('Request') + '<p>';
 	for (i in req) {
 		if (req.hasOwnProperty(i)) {
 			outstr += '' + i + ': ' + req[i] + '<br>';
 		}
 	}
-	outstr += '</p>'
-		+ m.bodyend;
+	outstr += '</p>' + m.bodyend;
+	return outstr;
+}
+
+m.hello = function (req) {
+	var hello = 'Hello, World!';
+	return m.headbegin
+			+ m.title(hello)
+			+ m.headend
+			+ m.bodybegin
+			+ '<h1>' + hello + '</h1>'
+			+ m.bodyend;
+
+}
+
+m.parse = function (str) {
+	return str.split('/')[1];
+};
+
+m.default = m.root;
+m.table = {'hello': m.hello};
+
+m.dispatch = function (req, res) {
+	var   component = m.parse(url.parse(req.url).pathname)
+		, answer = (component in m.table) ?
+			  m.table[component](req)
+			: m.default(req)
+		;
 	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.end(outstr);
+	res.end(answer);
 };
 	
 http.createServer(m.dispatch).listen(m.port, m.host);
